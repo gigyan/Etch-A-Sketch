5239250372a42168b3bb4fcae78388afddb79b90
gigyan <gyasiigyan13@gmail.com>
2023-01-08 19:32:31 -0500

    Improved drawing functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector("#container");
 
+
 //loop to create 256 squares to put inside container
 for(let i = 0; i < 256; i++)
 {
@@ -9,6 +10,42 @@ div.classList.add("square");
 
 container.appendChild(div);
 }
+
+const button = document.querySelectorAll(".square");
+
+let squareArray = Array.from(button);
+
+function changeColor(event){
+   
+}
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function changeColor(event){
+    if( event.type === "mouseenter" && mouseDown){
+        event.target.style.backgroundColor = "white";
+    }
+};
+
+squareArray.forEach(item => {
+    item.addEventListener("mouseenter" , changeColor);
+    item.addEventListener("mousedown", changeColor);
+});
+
+// squareArray.forEach(item => {
+// item.addEventListener("mouseenter" , () => {
+//     item.style.cssText = " background-color: white;";
+
+// });
+
+//     });
+
+
+
+
+
 
 
 
